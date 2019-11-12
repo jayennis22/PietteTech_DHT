9145d6bca71b18863ff9b72e485258dec572af91
ScruffR <ScruffR@users.noreply.github.com>
2019-11-12 13:57:35 +0100

    remove block quotes and adjust version number

--- src/PietteTech_DHT.cpp
+++ src/PietteTech_DHT.cpp
@@ -1,59 +1,53 @@
-/*
- * FILE:        PietteTech_DHT.cpp
- * VERSION:     0.4
- * PURPOSE:     Spark Interrupt driven lib for DHT sensors
- * LICENSE:     GPL v3 (http://www.gnu.org/licenses/gpl.html)
- *
- * S Piette (Piette Technologies) scott.piette@gmail.com
- *      January 2014        Original Spark Port
- *      October 2014        Added support for DHT21/22 sensors
- *                          Improved timing, moved FP math out of ISR
- *      September 2016      Updated for Particle and removed dependency
- *                          on callback_wrapper.  Use of callback_wrapper
- *                          is still for backward compatibility but not used
- *
- *                          is still for backward compatibility but not used
- * ScruffR
- *      February 2017       Migrated for Libraries 2.0
- *                          Fixed blocking acquireAndWait()
- *                          and previously ignored timeout setting
- *      January  2019       Updated timing for Particle Mesh devices
- *                          issue: https://github.com/particle-iot/device-os/issues/1654
- *                          
- * Based on adaptation by niesteszeck (github/niesteszeck)
- * Based on original DHT11 library (http://playgroudn.adruino.cc/Main/DHT11Lib)
- *
- *
- * With this library connect the DHT sensor to the following pins
- * Spark Core: D0, D1, D2, D3, D4, A0, A1, A3, A5, A6, A7
- * Particle  : any Pin but D0 & A5
- * See docs for more background
- *   https://docs.particle.io/reference/firmware/photon/#attachinterrupt-
- */
+// FILE:        PietteTech_DHT.h
+// VERSION:     0.0.11
+// PURPOSE:     Particle Interrupt driven lib for DHT sensors
+// LICENSE:     GPL v3 (http://www.gnu.org/licenses/gpl.html)
+// 
+// S Piette (Piette Technologies) scott.piette@gmail.com
+//      January 2014        Original Spark Port
+//      October 2014        Added support for DHT21/22 sensors
+//                          Improved timing, moved FP math out of ISR
+//      September 2016      Updated for Particle and removed dependency
+//                          on callback_wrapper.  Use of callback_wrapper
+//                          is still for backward compatibility but not used
+// ScruffR
+//      February 2017       Migrated for Libraries 2.0
+//                          Fixed blocking acquireAndWait()
+//                          and previously ignored timeout setting
+//      January  2019       Updated timing for Particle Mesh devices
+//                          issue: https://github.com/particle-iot/device-os/issues/1654
+//      November 2019       Incorporate workaround for SOS+14 bug
+//                          https://github.com/eliteio/PietteTech_DHT/issues/1
+// 
+// Based on adaptation by niesteszeck (github/niesteszeck)
+// Based on original DHT11 library (http://playgroudn.adruino.cc/Main/DHT11Lib)
+// 
+// With this library connect the DHT sensor to the interrupt enabled pins
+// See docs for more background
+//   https://docs.particle.io/reference/firmware/photon/#attachinterrupt-
 
- /*
-     Timing of DHT22 SDA signal line after MCU pulls low for 1ms
-     https://github.com/mtnscott/Spark_DHT/AM2302.pdf
+//    Timing of DHT22 SDA signal line after MCU pulls low for 1ms
+//    https://github.com/mtnscott/Spark_DHT/AM2302.pdf
+//
+//  - - - -            -----           -- - - --            ------- - -
+//         \          /     \         /  \      \          /
+//          +        /       +       /    +      +        /
+//           \      /         \     /      \      \      /
+//            ------           -----        -- - --------
+// ^        ^                ^                   ^          ^
+// |   Ts   |        Tr      |        Td         |    Te    |
+//
+//    Ts : Start time from MCU changing SDA from Output High to Tri-State (Hi-Z)
+//         Spec: 20-200us             Tested: < 65us
+//    Tr : DHT response to MCU controlling SDA and pulling Low and High to
+//         start of first data bit
+//         Spec: 150-170us            Tested: 125 - 200us
+//    Td : DHT data bit, falling edge to falling edge
+//         Spec: '0' 70us - 85us      Tested: 60 - 110us
+//         Spec: '1' 116us - 130us    Tested: 111 - 155us
+//    Te : DHT releases SDA to Tri-State (Hi-Z)
+//         Spec: 45-55us              Not Tested
 
-   - - - -            -----           -- - - --            ------- - -
-          \          /     \         /  \      \          /
-           +        /       +       /    +      +        /
-            \      /         \     /      \      \      /
-             ------           -----        -- - --------
-  ^        ^                ^                   ^          ^
-  |   Ts   |        Tr      |        Td         |    Te    |
-
-     Ts : Start time from MCU changing SDA from Output High to Tri-State (Hi-Z)
-          Spec: 20-200us             Tested: < 65us
-     Tr : DHT response to MCU controlling SDA and pulling Low and High to
-          start of first data bit
-          Spec: 150-170us            Tested: 125 - 200us
-     Td : DHT data bit, falling edge to falling edge
-          Spec: '0' 70us - 85us      Tested: 60 - 110us
-          Spec: '1' 116us - 130us    Tested: 111 - 155us
-     Te : DHT releases SDA to Tri-State (Hi-Z)
-          Spec: 45-55us              Not Tested
-  */
 
 #include "PietteTech_DHT.h"
 
@@ -66,10 +60,10 @@ uint16_t word(uint8_t high, uint8_t low) {
 }
 #endif
 
-/*
- * NOTE:  callback_wrapper is only here for backwards compatibility with v0.3 and earlier
- *        it is no longer used or needed
- */
+// 
+// NOTE:  callback_wrapper is only here for backwards compatibility with v0.3 and earlier
+//        it is no longer used or needed
+// 
 PietteTech_DHT::PietteTech_DHT() {
 }
 PietteTech_DHT::PietteTech_DHT(uint8_t sigPin, uint8_t dht_type, void(*callback_wrapper)()) {
@@ -77,10 +71,10 @@ PietteTech_DHT::PietteTech_DHT(uint8_t sigPin, uint8_t dht_type, void(*callback_
   _type = dht_type;
 }
 
-/*
- * NOTE:  callback_wrapper is only here for backwards compatibility with v0.3 and earlier
- *        it is no longer used or needed
- */
+// 
+// NOTE:  callback_wrapper is only here for backwards compatibility with v0.3 and earlier
+//        it is no longer used or needed
+// 
 void PietteTech_DHT::begin() {
   _firstreading = true;
   _lastreadtime = 0;
@@ -111,34 +105,34 @@ int PietteTech_DHT::acquire() {
   }
 
   if (_state == STOPPED || _state == ACQUIRED) {
-    /*
-     * Setup the initial state machine
-     */
+    // 
+    // Setup the initial state machine
+    // 
     _firstreading = false;
     _lastreadtime = currenttime;
     _state = RESPONSE;
 
 #if defined(DHT_DEBUG_TIMING)
-    /*
-     * Clear the debug timings array
-     */
+    // 
+    // Clear the debug timings array
+    // 
     for (int i = 0; i < 41; i++) _edges[i] = 0;
     _e = &_edges[0];
 #endif
 
-    /*
-     * Set the initial values in the buffer and variables
-     */
+    // 
+    // Set the initial values in the buffer and variables
+    // 
     for (int i = 0; i < 5; i++) _bits[i] = 0;
     _cnt = 7;
     _idx = 0;
     _hum = 0;
     _temp = 0;
 
-    /*
-     * Toggle the digital output to trigger the DHT device
-     * to send us temperature and humidity data
-     */
+    // 
+    // Toggle the digital output to trigger the DHT device
+    // to send us temperature and humidity data
+    // 
     pinMode(_sigPin, OUTPUT);
     digitalWrite(_sigPin, LOW);
     if (_type == DHT11)
@@ -147,10 +141,10 @@ int PietteTech_DHT::acquire() {
       delayMicroseconds(1500);    // DHT22 Spec: 0.8-20ms, 1ms typ
     pinMode(_sigPin, INPUT);        // Note Hi-Z mode with pullup resistor
                                     // will keep this high until the DHT responds.
-    /*
-     * Attach the interrupt handler to receive the data once the DHT
-     * starts to send us data
-     */
+    // 
+    // Attach the interrupt handler to receive the data once the DHT
+    // starts to send us data
+    // 
     _us = micros();
 #if (SYSTEM_VERSION < SYSTEM_VERSION_v121RC3)
     // no extra steps required
@@ -176,10 +170,10 @@ int PietteTech_DHT::acquireAndWait(uint32_t timeout) {
   return getStatus();
 }
 
-/*
- * NOTE:  isrCallback is only here for backwards compatibility with v0.3 and earlier
- *        it is no longer used or needed
- */
+// 
+// NOTE:  isrCallback is only here for backwards compatibility with v0.3 and earlier
+//        it is no longer used or needed
+// 
 void PietteTech_DHT::isrCallback() { }
 
 void PietteTech_DHT::_isrCallback() {
@@ -211,7 +205,7 @@ void PietteTech_DHT::_isrCallback() {
   }
   switch (_state) {
   case RESPONSE:            // Spec: 80us LOW followed by 80us HIGH
-    if (delta < 65) {      // Spec: 20-200us to first falling edge of response
+    if (delta < 65) {       // Spec: 20-200us to first falling edge of response
       _us -= delta;
       break; //do nothing, it started the response signal
       
@@ -347,9 +341,9 @@ float PietteTech_DHT::getKelvin() {
   return _temp + 273.15;
 }
 
-/*
- * Added methods for supporting Adafruit Unified Sensor framework
- */
+// 
+// Added methods for supporting Adafruit Unified Sensor framework
+// 
 float PietteTech_DHT::readTemperature() {
   acquireAndWait();
   return getCelsius();
